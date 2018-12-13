b09cc096b29d330ed6330dd7d3b19975e7ca6b05
gbagarella-massgov <32549081+gbagarella-massgov@users.noreply.github.com>
2018-12-13 10:56:07 -0500

    Update benefits-calculator-v1.js

--- benefits-calculator-v1.js
+++ benefits-calculator-v1.js
@@ -15,6 +15,21 @@ function getBenefits()
 return benefits;
 }
 
+function getDisregard()
+{
+    //Assume form with id="benCalc"
+    var theForm = document.forms["benCalc"];
+    var benefits_determination = theForm.elements["benefits_determination"];
+    var earnings_disregard = 0;
+    //If the textbox is not blank
+    if(benefits_determination.value != "")
+    {
+        earnings_disregard = (1/3) * parseInt(benefits_determination.value);
+    }
+    console.log(benefits);
+return benefits;
+}
+
 function getEarnings()
 {
     //Assume form with id="benCalc"
@@ -30,13 +45,13 @@ function getEarnings()
 return earnings;
 }
 
-
 function calcBenefits()
 {
     //Here we get the total price by calling our function
     //Each function returns a number so by calling them we add the values they return together
     if (getEarnings() > ((1/3) * (getBenefits())))
     {
+        var earnings_disregard = getDisregard()
         var benefits = getBenefits() - (getEarnings() - ((1/3) * (getBenefits())));
         var earnings = getEarnings();
         var take_home = benefits + earnings;
@@ -47,6 +62,7 @@ function calcBenefits()
         var earnings = getEarnings();
         var take_home = benefits + earnings;
 }
+    document.getElementById('earnings_disregard').value = earnings_disregard
     document.getElementById('earnings').value = earnings
     document.getElementById('benefits').value = benefits
     document.getElementById('take_home').value = take_home
