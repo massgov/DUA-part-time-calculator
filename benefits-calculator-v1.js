fd0cc520829cff033670596ebe88dec8fad569a9
gbagarella-massgov <32549081+gbagarella-massgov@users.noreply.github.com>
2018-12-13 11:05:22 -0500

    Update benefits-calculator-v1.js

--- benefits-calculator-v1.js
+++ benefits-calculator-v1.js
@@ -51,14 +51,14 @@ function calcBenefits()
     //Each function returns a number so by calling them we add the values they return together
     if (getEarnings() > ((1/3) * (getBenefits())))
     {
-        var earnings_disregard = getDisregard()
+        //var earnings_disregard = getDisregard()
         var benefits = getBenefits() - (getEarnings() - ((1/3) * (getBenefits())));
         var earnings = getEarnings();
         var take_home = benefits + earnings;
  }
  else
  {
-        var earnings_disregard = getDisregard()   
+        //var earnings_disregard = getDisregard()   
         var benefits = getBenefits();
         var earnings = getEarnings();
         var take_home = benefits + earnings;
